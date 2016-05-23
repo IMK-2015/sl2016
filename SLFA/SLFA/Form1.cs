b5d7f5b59b8cc9b057562ca03c47045acf81b95e
IMK-2015 <imkcamel@gmail.com>
2016-05-24 02:12:23 +0600

    First step

---
+++ SLFA/SLFA/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SLFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void создатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form f = new Form();
+            f.Name = "Resultat";
+            f.Text = "Результаты";
+            f.MdiParent = this;
+
+            RichTextBox rtb = new RichTextBox();
+            f.Controls.Add(rtb);
+            rtb.Dock = DockStyle.Fill;
+            f.Show();
+        }
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ActiveMdiChild != null)
+                ActiveMdiChild.Close();
+        }
+
+        private void закрытьВсеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form[] form = MdiChildren;
+            foreach (Form f in form)
+                f.Close();
+        }
+    }
+}
